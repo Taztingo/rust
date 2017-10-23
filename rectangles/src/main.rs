a930a5ddff68d234c0baea309a70db6b474120f5
Witkowski <matthew.witkowski@dev0074u16.g3ti.local>
2017-10-23 18:32:24 -0400

    Updated project to contain methods instead.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,16 +4,33 @@ struct Rectangle {
     width: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
+}
+
 fn main() {
     let rect = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 40, width: 10 };
+    let rect3 = Rectangle { length: 45, width: 60 };
 
     println!(
         "The area of a rectangle is {} square pixels.",
-        area(&rect)
+        rect.area()
     );
-    println!("Attempting to print the rectangle {:#?}" , rect);
-}
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.length * rect.width
+    println!("Can rect hold rect2? {}", rect.can_hold(&rect2));
+    println!("Can rect hold rect3? {}", rect.can_hold(&rect3));
+
+    let square = Rectangle::square(32);
+    println!("The internals of square are {:#?}", square);
 }
