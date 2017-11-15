631a695ae7c9e5e268c5c09e852061fbc9dc015f
Witkowski <matthew.witkowski@dev0074u16.g3ti.local>
2017-11-15 18:55:41 -0500

    Did an exercise that calculates the mean/median/mode of a data set that is given by user input.

---
+++ map_exercises/src/main.rs
@@ -0,0 +1,52 @@
+use std::io;
+
+fn main() {
+    println!("Enter a list of numbers separated by a space");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+    let mut list: Vec<i32> = Vec::new();
+    for number in input.split_whitespace() {
+        list.push(number.parse::<i32>().unwrap());
+    }
+    println!("The mean is: {}", calculate_mean(&list));
+    println!("The median is: {}", calculate_median(&list));
+    println!("The mode is: {}", calculate_mode(&list));
+}
+
+fn calculate_mean(list: &Vec<i32>) -> f32 {
+    let mut sum = 0;
+    for x in list.iter() {
+        sum += *x;
+    }
+   (sum as f32) / (list.len() as f32)
+}
+
+fn calculate_median(list: &Vec<i32>) -> f32 {
+    let mut copy = list.clone();
+    copy.sort();
+    let middle = ((copy.len() / 2) as usize);
+    let mut median = 0.0;
+    if copy.len() % 2 == 0 {
+        median = ((copy[middle] + copy[middle-1]) as f32) / 2.0;
+    } else {
+        median = copy[middle] as f32;
+    }
+    median
+}
+
+fn calculate_mode(list: &Vec<i32>) -> i32 {
+    use std::collections::HashMap;
+    let mut map = HashMap::new();
+    let mut max = -1;
+    let mut mode = -1;
+    for x in list.iter() {
+        let count = map.entry(*x).or_insert(0);
+        *count += 1;
+        if *count > max {
+            max = *count;
+            mode = *x;
+        }
+    }
+    mode
+}
