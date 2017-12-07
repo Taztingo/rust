01467594dedf881fc728ebe4eade02682f794786
Witkowski <matthew.witkowski@dev0074u16.g3ti.local>
2017-12-06 23:29:24 -0500

    Finished grepping of program, and used TDD to add final function.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -2,11 +2,35 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
+
 pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename)?;
+
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
-    println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
@@ -26,4 +50,16 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+
+    results
 }
