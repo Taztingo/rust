21be4063c62e7efdf987d380864dd04c7793bb55
Witkowski <matthew.witkowski@dev0074u16.g3ti.local>
2017-12-20 14:06:19 -0500

    Created a log sample to figure out how logging works in rust.

--- log_sample/src/main.rs
+++ log_sample/src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 fn main() {
+    env_logger::init().unwrap();
+
+    info!("Starting up");
+    error!("Error");
     println!("Hello, world!");
 }
