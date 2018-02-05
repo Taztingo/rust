26efb8eae319fed5ecf1ff402a8aa49a86137a5d
Witkowski <matthew.witkowski@dev0074u16.g3ti.local>
2018-02-05 12:55:07 -0500

    Created a shell project. It's usage is to emulate the Linux shell.

---
+++ shell/src/main.rs
@@ -0,0 +1,49 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    display_welcome();
+    run_main_loop();
+}
+
+fn run_main_loop() {
+    let running = true;
+    while running {
+        match get_user_input() {
+            Some(input) => {
+                parse_line(&input);
+            },
+            None => {}
+        }
+    }
+}
+
+fn display_welcome() {
+    println!("Welcome to the Rust Shell.\n");
+}
+
+fn get_user_input() -> Option<String> {
+    let mut input = String::new();
+    print!(">> ");
+    io::stdout().flush();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            Some(input)
+        },
+        Err(error) => {
+            println!("Error unable to read line. {}", error);
+            None
+        }
+    }
+}
+
+fn parse_line(line: &String) {
+
+    //match line {
+
+    //};
+}
+
+fn parse_command(command: String, args: Vec<String>) {
+
+}
