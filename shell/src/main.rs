8ab1c5f9afd4f66580b670c25877e625ca38006c
Witkowski <matthew.witkowski@dev0074u16.g3ti.local>
2018-02-26 13:02:45 -0500

    Updated to split my if statement into multiple strategies.

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -44,125 +44,293 @@ fn parse_line(line: String) {
     parse_command(command, args);
 }
 
-fn help_command() {
-    println!("Rust bash, version 1.00");
-    println!("These shell commands are defined interally. Type `help` to see this list.");
-    println!("A star (*) next to a name means that the command is disabled\n");
-
-    println!("help\t\t\tDisplays the help menu");
-    println!("echo\t\t\tOutputs the passed in message");
+pub trait Strategy {
+    fn execute(&self, args: &mut Vec<String>);
 }
 
-fn echo_command(message: &str) {
-    println!("{}", message);
-}
+////////////////////////////////////////////////////////
+struct RmStrategy {}
 
-use std::fs;
-fn ls_command() {
-    let paths = fs::read_dir("./").unwrap();
-
-    for path in paths {
-        print!("{}\t", path.unwrap().path().display());
+impl RmStrategy {
+    pub fn new() -> RmStrategy {
+        RmStrategy{}
     }
-    println!("");
+
+    fn rm_command(filename: &str, args: &mut Vec<String>) {
+        let action = |file: &str| fs::remove_file(file);
+        if args.len() > 0 {
+            match args[0].as_str() {
+                "-r" => {
+                    match fs::remove_dir_all(filename) {
+                        Ok(_) => {},
+                        Err(error) => println!("rm: cannot remove '{}': Is a directory", filename)
+                    }
+                }
+                _ => {}
+            }
+        } else {
+            match fs::remove_file(filename) {
+                Ok(_) => {},
+                Err(error) => println!("rm: cannot remove '{}': Is a directory", filename)
+            };
+        }
+    }
 }
 
-use std::fs::File;
-fn touch_command(filename: &str) {
-    match File::open(filename) {
-        Ok(f) => {},
-        Err(open_error) => { match File::create(filename) {
-            Ok(f) => {},
-            Err(create_error) => {}
-        }}
-    };
+impl Strategy for RmStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        RmStrategy::rm_command(args.remove(0).as_str(), args);
+    }
 }
+/////////////////////////////////////////////////////////////////////////////////////
 
-use std::env;
-use std::path::Path;
-fn cd_command(path: &str) {
-    let new_path = Path::new(path);
-    env::set_current_dir(&new_path).is_ok();
-}
-
-
-fn mkdir_command(path: &str) {
-    match fs::create_dir(path) {
-        Ok(_) => {},
-        Err(_) => println!("mkdir: cannot create directory '{}': File exists", path)
-    };
-}
+////////////////////////////////////////////////////////
+struct CatStrategy {}
 
 use std::io::Read;
-fn cat_command(filename: &str) {
-    match File::open(filename) {
-        Ok(mut file) => {
-            let mut contents = String::new();
-            file.read_to_string(&mut contents);
-            println!("{}\n", contents);
-        },
-        Err(_) => {
-            println!("File does not exist!");
+use std::fs::File;
+impl CatStrategy {
+    pub fn new() -> CatStrategy {
+        CatStrategy{}
+    }
+
+    fn cat_command(filename: &str) {
+        match File::open(filename) {
+            Ok(mut file) => {
+                let mut contents = String::new();
+                file.read_to_string(&mut contents);
+                println!("{}\n", contents);
+            },
+            Err(_) => {
+                println!("File does not exist!");
+            }
         }
     }
 }
 
-fn exit_command() {
-    println!("Not yet implemented!");
+impl Strategy for CatStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        CatStrategy::cat_command(args.remove(0).as_str());
+    }
 }
+/////////////////////////////////////////////////////////////////////////////////////
 
-fn rm_command(filename: &str, args: Vec<&str>) {
-    let action = |file: &str| fs::remove_file(file);
-    if args.len() > 0 {
-        match args[0] {
-            "-r" => {
-                match fs::remove_dir_all(filename) {
-                    Ok(_) => {},
-                    Err(error) => println!("rm: cannot remove '{}': Is a directory", filename)
-                }
-            }
-            _ => {}
-        }
-    } else {
-        match fs::remove_file(filename) {
+////////////////////////////////////////////////////////
+struct MkdirStrategy {}
+
+impl MkdirStrategy {
+    pub fn new() -> MkdirStrategy {
+        MkdirStrategy{}
+    }
+
+    fn mkdir_command(path: &str) {
+        match fs::create_dir(path) {
             Ok(_) => {},
-            Err(error) => println!("rm: cannot remove '{}': Is a directory", filename)
+            Err(_) => println!("mkdir: cannot create directory '{}': File exists", path)
         };
     }
 }
 
-fn parse_command(command: &str, mut args: Vec<&str>) {
-    match command {
-        "help" => {
-            help_command();
-        },
-        "echo" => {
-            echo_command(args[0]);
-        },
-        "ls" => {
-            ls_command();
-        },
-        "touch" => {
-            touch_command(args[0]);
-        },
-        "cd" => {
-            cd_command(args[0]);
-        },
-        "mkdir" => {
-            mkdir_command(args[0]);
-        },
-        "cat" => {
-            cat_command(args[0]);
-        },
-        "exit" => {
-            exit_command();
-        },
-        "rm" => {
-            rm_command(args.remove(0), args);
+impl Strategy for MkdirStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        MkdirStrategy::mkdir_command(args.remove(0).as_str());
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+struct CdStrategy {}
+
+use std::env;
+impl CdStrategy {
+    pub fn new() -> CdStrategy {
+        CdStrategy{}
+    }
+
+    fn cd_command(path: &str) {
+        let new_path = Path::new(path);
+        env::set_current_dir(&new_path).is_ok();
+    }
+}
+
+impl Strategy for CdStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        CdStrategy::cd_command(args.remove(0).as_str());
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+struct HelpStrategy {}
+
+use std::path::Path;
+impl HelpStrategy {
+    pub fn new() -> HelpStrategy {
+        HelpStrategy{}
+    }
+
+    fn help_command() {
+        println!("Rust bash, version 1.00");
+        println!("These shell commands are defined interally. Type `help` to see this list.");
+        println!("A star (*) next to a name means that the command is disabled\n");
+
+        println!("help\t\t\tDisplays the help menu");
+        println!("echo\t\t\tOutputs the passed in message");
+    }
+}
+
+impl Strategy for HelpStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        HelpStrategy::help_command();
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+struct LsStrategy {}
+use std::fs;
+impl LsStrategy {
+    pub fn new() -> LsStrategy {
+        LsStrategy{}
+    }
+
+    fn ls_command() {
+        let paths = fs::read_dir("./").unwrap();
+
+        for path in paths {
+            print!("{}\t", path.unwrap().path().display());
         }
-        _ => { 
-            println!("{}: command not found", command);
-            io::stdout().flush();
+        println!("");
+    }
+}
+
+impl Strategy for LsStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        LsStrategy::ls_command();
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+struct EchoStrategy {}
+
+impl EchoStrategy {
+    pub fn new() -> EchoStrategy {
+        EchoStrategy{}
+    }
+
+    fn echo_command(message: &str) {
+        println!("{}", message);
+    }
+}
+
+impl Strategy for EchoStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        EchoStrategy::echo_command(args.remove(0).as_str());
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+struct TouchStrategy {}
+
+impl TouchStrategy {
+    pub fn new() -> TouchStrategy {
+        TouchStrategy{}
+    }
+
+    fn touch_command(filename: &str) {
+        match File::open(filename) {
+            Ok(f) => {},
+            Err(open_error) => { match File::create(filename) {
+                Ok(f) => {},
+                Err(create_error) => {}
+            }}
+        };
+    }
+}
+
+impl Strategy for TouchStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        TouchStrategy::touch_command(args.remove(0).as_str());
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+struct UnknownStrategy {}
+
+impl UnknownStrategy {
+    pub fn new() -> UnknownStrategy {
+        UnknownStrategy{}
+    }
+}
+
+impl Strategy for UnknownStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+struct ExitStrategy {}
+
+impl ExitStrategy {
+    pub fn new() -> ExitStrategy {
+        ExitStrategy{}
+    }
+
+    fn exit_command() {
+        println!("Not yet implemented!");
+    }
+}
+
+impl Strategy for ExitStrategy {
+    fn execute(&self, mut args: &mut Vec<String>) {
+        ExitStrategy::exit_command();
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+struct Command {
+    args: Vec<String>,
+    strategy: Box<Strategy>
+}
+
+impl Command {
+    pub fn new() -> Command {
+        Command {
+            strategy: Box::new(UnknownStrategy::new()),
+            args: Vec::new()
         }
     }
+
+    pub fn parse(&mut self, command: &str, args: Vec<&str>) {
+        self.strategy = match command {
+            "help" => Box::new(HelpStrategy::new()),
+            "echo" => Box::new(EchoStrategy::new()),
+            "ls" => Box::new(LsStrategy::new()),
+            "touch" => Box::new(TouchStrategy::new()),
+            "cd" => Box::new(CdStrategy::new()),
+            "mkdir" => Box::new(MkdirStrategy::new()),
+            "cat" => Box::new(CatStrategy::new()),
+            "exit" => Box::new(ExitStrategy::new()),
+            "rm" => Box::new(RmStrategy::new()),
+            _ => Box::new(UnknownStrategy::new())
+        };
+        args.iter().map(|arg| self.args.push(String::from(*arg)));
+    }
+
+    pub fn execute(&mut self) {
+        self.strategy.execute(&mut self.args)
+    }
+}
+
+
+fn parse_command(command: &str, mut args: Vec<&str>) {
+    let mut new_command = Command::new();
+    new_command.parse(command, args);
+    new_command.execute();
 }
